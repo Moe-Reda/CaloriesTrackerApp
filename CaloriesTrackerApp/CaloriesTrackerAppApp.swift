d665ce7cb6b0d8c917ea766fc964c8678719dd60
Moe-Reda <108328374+Moe-Reda@users.noreply.github.com>
2023-05-29 03:02:38 -0400

    Initial Commit

---
+++ CaloriesTrackerApp/CaloriesTrackerAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  CaloriesTrackerAppApp.swift
+//  CaloriesTrackerApp
+//
+//  Created by Mohamed Reda Mahboub on 2023-05-29.
+//
+
+import SwiftUI
+
+@main
+struct CaloriesTrackerAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
